5dd0e3e686d758343a9cef3279c5d5331ea9cc17
sam0307204 <sam0307204@gmail.com>
2024-10-08 16:44:01 +0530

    Add none scroll and change in page grid

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Chat.css'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
+import ChatBox from '../../components/ChatBox/ChatBox'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
 
 const Chat = () => {
   return (
-    <div>Chat</div>
+    <div className="chat">
+        <div className="chat-container">
+            <LeftSidebar />
+            <ChatBox />
+            <RightSidebar />
+        </div>
+    </div>
   )
 }
 
